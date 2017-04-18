cab89b38c0eb70ddf02279362181441cf4622573
Matthieu Gautier <mgautier@kymeria.fr>
2017-04-18 10:42:38 +0200

    Publish intermediate dependencies in a http accessible directory.
    Those dependencies will be used by travis CI in other projects.
    We want to avoid other project to need a ssh key to get them :
    - To avoid spread(and maintenance) of ssh key.
    - For PR coming from another repositories, ssh key is not available.
    
    By making those dependencies available by http, we simplify the process.
    
    Fix partially #34.
    Archives download need to be updated in client projects.

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -12,7 +12,7 @@ if [[ "x$DEPS_ARCHIVES" != "x" ]]
 then
   scp -vp -i ${SSH_KEY} \
     ${DEPS_ARCHIVES} \
-    nightlybot@download.kiwix.org:~/travis_deps/
+    nightlybot@download.kiwix.org:/var/www/tmp.kiwix.org/ci/
 fi
 
 NIGHTLY_ARCHIVES=$(find $NIGHTLY_ARCHIVES_DIR -type f)
