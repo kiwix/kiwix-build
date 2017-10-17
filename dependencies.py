00bc8238f075322b879fe494657e9578dbdac34d
Matthieu Gautier <mgautier@kymeria.fr>
2017-10-17 14:53:51 +0200

    Add support of `zim-tools` in kiwix-build.

--- dependencies.py
+++ dependencies.py
@@ -262,6 +262,21 @@ class Libzim(Dependency):
 
     Builder = MesonBuilder
 
+class ZimTools(Dependency):
+    name = "zim-tools"
+    dependencies = ['libzim']
+
+    class Source(GitClone):
+        git_remote = "https://github.com/openzim/zim-tools.git"
+        git_dir = "zim-tools"
+
+    class Builder(MesonBuilder):
+        @property
+        def configure_option(self):
+            if self.buildEnv.platform_info.static:
+                return "-Dstatic-linkage=true"
+            return ""
+
 
 class Zimwriterfs(Dependency):
     name = "zimwriterfs"
