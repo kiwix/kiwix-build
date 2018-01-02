14d465150469acda921730781ab7340e078076eb
Isaac Hutt <mhutti1@gmail.com>
2018-01-02 22:51:11 +0000

    Fix Android APK output location

--- travis/compile_all.sh
+++ travis/compile_all.sh
@@ -119,7 +119,7 @@ EOF
       ;;
     android_*)
       APK_NAME="kiwix-${PLATFORM}"
-      cp ${BASE_DIR}/kiwix-android/app/build/outputs/apk/kiwix/release/app-kiwix-debug.apk ${NIGHTLY_ARCHIVES_DIR}/${APK_NAME}-debug.apk
+      cp ${BASE_DIR}/kiwix-android/app/build/outputs/apk/kiwix/debug/app-kiwix-debug.apk ${NIGHTLY_ARCHIVES_DIR}/${APK_NAME}-debug.apk
       cp ${BASE_DIR}/kiwix-android/app/build/outputs/apk/kiwix/release/app-kiwix-release-unsigned.apk ${NIGHTLY_ARCHIVES_DIR}/${APK_NAME}-release-unsigned.apk
       ;;
   esac
