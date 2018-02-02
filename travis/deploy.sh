cb468fc49f7bf209fe3f42334b88e7df7556db88
Matthieu Gautier <mgautier@kymeria.fr>
2018-02-02 12:07:01 +0100

    We must deploy releases archive when a tag is set not on cron.

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -8,35 +8,40 @@ DIST_KIWIX_ARCHIVES_DIR=${HOME}/DIST_KIWIX_ARCHIVES
 DIST_ZIM_ARCHIVES_DIR=${HOME}/DIST_ZIM_ARCHIVES
 SSH_KEY=travis/travisci_builder_id_key
 
-NIGHTLY_ARCHIVES=$(find $NIGHTLY_ARCHIVES_DIR -type f)
-if [[ "x$NIGHTLY_ARCHIVES" != "x" ]]
+if [[ "$TRAVIS_EVENT_TYPE" = "cron" ]]
 then
-  scp -vrp -i ${SSH_KEY} \
-    ${NIGHTLY_ARCHIVES} \
-    nightlybot@download.kiwix.org:/var/www/download.kiwix.org/nightly/$(date +%Y-%m-%d)
+  NIGHTLY_ARCHIVES=$(find $NIGHTLY_ARCHIVES_DIR -type f)
+  if [[ "x$NIGHTLY_ARCHIVES" != "x" ]]
+  then
+    scp -vrp -i ${SSH_KEY} \
+      ${NIGHTLY_ARCHIVES} \
+      nightlybot@download.kiwix.org:/var/www/download.kiwix.org/nightly/$(date +%Y-%m-%d)
+  fi
 fi
 
-RELEASE_ARCHIVES=$(find $RELEASE_ARCHIVES_DIR -type f)
-if [[ "x$RELEASE_ARCHIVES" != "x" ]]
-then
-  scp -vrp -i ${SSH_KEY} \
-    ${RELEASE_ARCHIVES} \
-    nightlybot@downoald.kiwix.org:/var/www/download.kiwix.org/releases
-fi
+if [[ "x$TRAVIS_TAG" != "x" ]]
+  RELEASE_ARCHIVES=$(find $RELEASE_ARCHIVES_DIR -type f)
+  if [[ "x$RELEASE_ARCHIVES" != "x" ]]
+  then
+    scp -vrp -i ${SSH_KEY} \
+      ${RELEASE_ARCHIVES} \
+      nightlybot@downoald.kiwix.org:/var/www/download.kiwix.org/releases
+  fi
 
-DIST_KIWIX_ARCHIVES=$(find $DIST_KIWIX_ARCHIVES_DIR -type f)
-if [[ "x$DIST_KIWIX_ARCHIVES" != "x" ]]
-then
-  scp -vrp -i ${SSH_KEY} \
-    ${DIST_KIWIX_ARCHIVES} \
-    nightlybot@download.kiwix.org:/var/www/download.kiwix.org/release
-fi
+  DIST_KIWIX_ARCHIVES=$(find $DIST_KIWIX_ARCHIVES_DIR -type f)
+  if [[ "x$DIST_KIWIX_ARCHIVES" != "x" ]]
+  then
+    scp -vrp -i ${SSH_KEY} \
+      ${DIST_KIWIX_ARCHIVES} \
+      nightlybot@download.kiwix.org:/var/www/download.kiwix.org/release
+  fi
 
-DIST_ZIM_ARCHIVES=$(find $DIST_ZIM_ARCHIVES_DIR -type f)
-if [[ "x$DIST_ZIM_ARCHIVES" != "x" ]]
-then
-  scp -vrp -i ${SSH_KEY} \
-    ${DIST_ZIM_ARCHIVES} \
-    nightlybot@download.openzim.org:/var/www/download.openzim.org/release
+  DIST_ZIM_ARCHIVES=$(find $DIST_ZIM_ARCHIVES_DIR -type f)
+  if [[ "x$DIST_ZIM_ARCHIVES" != "x" ]]
+  then
+    scp -vrp -i ${SSH_KEY} \
+      ${DIST_ZIM_ARCHIVES} \
+      nightlybot@download.openzim.org:/var/www/download.openzim.org/release
+  fi
 fi
 
