d93fc9dc9c156ac5934b020c8194361d9a608c47
Matthieu Gautier <mgautier@kymeria.fr>
2018-03-19 18:39:03 +0100

    Also cache pip in travis.

--- travis/install_extra_deps.sh
+++ travis/install_extra_deps.sh
@@ -6,8 +6,8 @@ orig_dir=$(pwd)
 
 sudo apt-get update -qq
 sudo apt-get install -qq python3-pip zlib1g-dev libjpeg-dev
-pip3 install --user meson==0.43.0
-pip3 install --user pillow
+pip3 install --user --upgrade pip wheel
+pip3 install --user pillow meson==0.43.0
 
 # ninja
 git clone git://github.com/ninja-build/ninja.git
