752a5d58ce614a12153a6fb9662bc8376dfd21aa
Matthieu Gautier <mgautier@kymeria.fr>
2018-03-20 10:12:51 +0100

    Do not do sudo command.
    - Install all packages using travis apt plugin
    - Copy ninja binary in $HOME/bin.
    
    We still need sudo required because travis image has more memory in this
    case and this is needed to test big cluster in libzim.

--- travis/install_extra_deps.sh
+++ travis/install_extra_deps.sh
@@ -4,15 +4,13 @@ set -e
 
 orig_dir=$(pwd)
 
-sudo apt-get update -qq
-sudo apt-get install -qq python3-pip zlib1g-dev libjpeg-dev
 pip3 install --user --upgrade pip wheel
 pip3 install --user pillow meson==0.43.0
 
 # ninja
 wget https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-linux.zip
 unzip ninja-linux.zip ninja
-sudo cp ninja /bin
+cp ninja $HOME/bin
 
 cd $orig_dir
 
