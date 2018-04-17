3ad73093f5d041927b1fa31b981de76036401a68
Matthieu Gautier <mgautier@kymeria.fr>
2018-04-17 17:41:08 +0200

    [TRAVISCI] Build kiwix-lib on macOS

--- travis/install_extra_deps.sh
+++ travis/install_extra_deps.sh
@@ -2,13 +2,24 @@
 
 set -e
 
-pip3 install --user --upgrade pip wheel
-pip3 install --user pillow
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]
+then
+  brew update
+  brew upgrade python3
+  pip3 install pillow
+  pip3 install .
 
-pip3 install --user .
+  wget https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-mac.zip
+  unzip ninja-mac.zip ninja
+else
+  pip3 install --user --upgrade pip wheel
+  pip3 install --user pillow
+  pip3 install --user .
 
-# ninja
-wget https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-linux.zip
-unzip ninja-linux.zip ninja
+  wget https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-linux.zip
+  unzip ninja-linux.zip ninja
+fi
+
+mkdir -p $HOME/bin
 cp ninja $HOME/bin
 
