0e05da4ff76bd5be04d40efee2f87e9fcce30de5
mhutti1 <mhutti1@gmail.com>
2018-06-26 00:22:53 +0100

    Fix vanllia build and deploy script

--- build_vanilla_app.pl
+++ build_vanilla_app.pl
@@ -39,44 +39,42 @@ $ENV{ORG_GRADLE_PROJECT_version_name} = $version_name;
 
 # Compile apps
 $ENV{ORG_GRADLE_PROJECT_version_code} = $version_code_base;
-$cmd = "kiwix-build --target-platform android_arm kiwix-android"; system $cmd;
+$cmd = "kiwix-build kiwix-android --android-arch arm"; system $cmd;
+
+$cmd = "./TOOLCHAINS/android-sdk-r25.2.3/build-tools/25.0.2/apksigner sign -ks \"${keystore}\" --ks-pass env:KIWIX_KEY --out signed_apks/app-$ENV{ORG_GRADLE_PROJECT_version_code}-release-signed.apk BUILD_android/kiwix-android/app/build/outputs/apk/kiwix/release/app-kiwix-release-unsigned.apk";
+system $cmd;
 
 $ENV{ORG_GRADLE_PROJECT_version_code} = "1" . $version_code_base;
-$cmd = "kiwix-build --target-platform android_arm64 kiwix-android"; system $cmd;
+$cmd = "kiwix-build kiwix-android --android-arch arm64"; system $cmd;
+
+$cmd = "./TOOLCHAINS/android-sdk-r25.2.3/build-tools/25.0.2/apksigner sign -ks \"${keystore}\" --ks-pass env:KIWIX_KEY --out signed_apks/app-$ENV{ORG_GRADLE_PROJECT_version_code}-release-signed.apk BUILD_android/kiwix-android/app/build/outputs/apk/kiwix/release/app-kiwix-release-unsigned.apk";
+system $cmd;
 
 $ENV{ORG_GRADLE_PROJECT_version_code} = "2" . $version_code_base;
-$cmd = "kiwix-build --target-platform android_x86 kiwix-android"; system $cmd;
+$cmd = "kiwix-build kiwix-android --android-arch x86"; system $cmd;
+
+$cmd = "./TOOLCHAINS/android-sdk-r25.2.3/build-tools/25.0.2/apksigner sign -ks \"${keystore}\" --ks-pass env:KIWIX_KEY --out signed_apks/app-$ENV{ORG_GRADLE_PROJECT_version_code}-release-signed.apk BUILD_android/kiwix-android/app/build/outputs/apk/kiwix/release/app-kiwix-release-unsigned.apk";
+system $cmd;
 
 $ENV{ORG_GRADLE_PROJECT_version_code} = "3" . $version_code_base;
-$cmd = "kiwix-build --target-platform android_x86_64 kiwix-android"; system $cmd;
+$cmd = "kiwix-build kiwix-android --android-arch x86_64"; system $cmd;
+
+$cmd = "./TOOLCHAINS/android-sdk-r25.2.3/build-tools/25.0.2/apksigner sign -ks \"${keystore}\" --ks-pass env:KIWIX_KEY --out signed_apks/app-$ENV{ORG_GRADLE_PROJECT_version_code}-release-signed.apk BUILD_android/kiwix-android/app/build/outputs/apk/kiwix/release/app-kiwix-release-unsigned.apk";
+system $cmd;
 
 $ENV{ORG_GRADLE_PROJECT_version_code} = "4" . $version_code_base;
-$cmd = "kiwix-build --target-platform android_mips kiwix-android"; system $cmd;
+$cmd = "kiwix-build kiwix-android --android-arch mips"; system $cmd;
+
+$cmd = "./TOOLCHAINS/android-sdk-r25.2.3/build-tools/25.0.2/apksigner sign -ks \"${keystore}\" --ks-pass env:KIWIX_KEY --out signed_apks/app-$ENV{ORG_GRADLE_PROJECT_version_code}-release-signed.apk BUILD_android/kiwix-android/app/build/outputs/apk/kiwix/release/app-kiwix-release-unsigned.apk";
+system $cmd;
 
 $ENV{ORG_GRADLE_PROJECT_version_code} = "5" . $version_code_base;
-$cmd = "kiwix-build --target-platform android_mips64 kiwix-android"; system $cmd;
+$cmd = "kiwix-build kiwix-android --android-arch mips64"; system $cmd;
 
-# Sign apps
-$cmd = "./TOOLCHAINS/android-sdk-r25.2.3/build-tools/25.0.2/apksigner sign -ks \"${keystore}\" --ks-pass env:KIWIX_KEY --out signed_apks/app-${version_code_base}-release-signed.apk BUILD_android_arm/kiwix-android/app/build/outputs/apk/kiwix/release/app-kiwix-release-unsigned.apk";
-system $cmd;
-
-$cmd = "./TOOLCHAINS/android-sdk-r25.2.3/build-tools/25.0.2/apksigner sign -ks \"${keystore}\" --ks-pass env:KIWIX_KEY --out signed_apks/app-1${version_code_base}-release-signed.apk BUILD_android_arm64/kiwix-android/app/build/outputs/apk/kiwix/release/app-kiwix-release-unsigned.apk";
-system $cmd;
-
-$cmd = "./TOOLCHAINS/android-sdk-r25.2.3/build-tools/25.0.2/apksigner sign -ks \"${keystore}\" --ks-pass env:KIWIX_KEY --out signed_apks/app-2${version_code_base}-release-signed.apk BUILD_android_x86/kiwix-android/app/build/outputs/apk/kiwix/release/app-kiwix-release-unsigned.apk";
-system $cmd;
-
-$cmd = "./TOOLCHAINS/android-sdk-r25.2.3/build-tools/25.0.2/apksigner sign -ks \"${keystore}\" --ks-pass env:KIWIX_KEY --out signed_apks/app-3${version_code_base}-release-signed.apk BUILD_android_x86_64/kiwix-android/app/build/outputs/apk/kiwix/release/app-kiwix-release-unsigned.apk";
-system $cmd;
-
-$cmd = "./TOOLCHAINS/android-sdk-r25.2.3/build-tools/25.0.2/apksigner sign -ks \"${keystore}\" --ks-pass env:KIWIX_KEY --out signed_apks/app-4${version_code_base}-release-signed.apk BUILD_android_mips/kiwix-android/app/build/outputs/apk/kiwix/release/app-kiwix-release-unsigned.apk";
-system $cmd;
-
-$cmd = "./TOOLCHAINS/android-sdk-r25.2.3/build-tools/25.0.2/apksigner sign -ks \"${keystore}\" --ks-pass env:KIWIX_KEY --out signed_apks/app-5${version_code_base}-release-signed.apk BUILD_android_mips64/kiwix-android/app/build/outputs/apk/kiwix/release/app-kiwix-release-unsigned.apk";
+$cmd = "./TOOLCHAINS/android-sdk-r25.2.3/build-tools/25.0.2/apksigner sign -ks \"${keystore}\" --ks-pass env:KIWIX_KEY --out signed_apks/app-$ENV{ORG_GRADLE_PROJECT_version_code}-release-signed.apk BUILD_android/kiwix-android/app/build/outputs/apk/kiwix/release/app-kiwix-release-unsigned.apk";
 system $cmd;
 
 # Upload
-
 $cmd = "./build_vanilla_app.py --step publish --google-api-key ${api_key} --apks-dir signed_apks";
 system $cmd;
 
