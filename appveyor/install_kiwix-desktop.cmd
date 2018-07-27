a565af2af48ca31a96f032136c72cfa32b15f021
Matthieu Gautier <mgautier@kymeria.fr>
2018-07-27 18:23:15 +0200

    [skip ci] WIP use kiwix-desktop window_utf16 branch

--- appveyor/install_kiwix-desktop.cmd
+++ appveyor/install_kiwix-desktop.cmd
@@ -2,6 +2,7 @@ REM ========================================================
 REM Install kiwix-desktop
 git clone https://github.com/kiwix/kiwix-desktop || exit /b 1
 cd kiwix-desktop
+git checkout windows_utf16
 echo "Getting fix_desktop"
 curl -fsSL -O http://public.kymeria.fr/KIWIX/windows/fix_desktop_makefile.py_ || exit /b 1
 echo "Running qmake"
