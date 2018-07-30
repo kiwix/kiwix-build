9d7e1af17f58150ca91ff46ec49a3988766752a9
Matthieu Gautier <mgautier@kymeria.fr>
2018-07-30 20:53:14 +0200

    `libzim`'s branch window has been merged in master.

--- appveyor/install_libzim.cmd
+++ appveyor/install_libzim.cmd
@@ -2,7 +2,6 @@ REM ========================================================
 REM Install libzim
 git clone https://github.com/openzim/libzim.git || exit /b 1
 cd libzim
-git checkout windows || exit /b 1
 meson.py . build --prefix %EXTRA_DIR% --default-library static --buildtype release || exit /b 1
 cd build
 ninja || exit /b 1
