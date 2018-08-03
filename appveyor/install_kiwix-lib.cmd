b910a7b7203941f61672f5ce4d7fbda42fa16703
Matthieu Gautier <mgautier@kymeria.fr>
2018-08-03 19:14:43 +0200

    [AppVeyor] No more windows branch for kiwix-lib.

--- appveyor/install_kiwix-lib.cmd
+++ appveyor/install_kiwix-lib.cmd
@@ -2,7 +2,6 @@ REM ========================================================
 REM Install kiwix-lib
 git clone https://github.com/kiwix/kiwix-lib.git || exit /b 1
 cd kiwix-lib
-git checkout windows || exit /b 1
 meson.py . build --prefix %EXTRA_DIR% --default-library static --buildtype release || exit /b 1
 cd build
 ninja || exit /b 1
