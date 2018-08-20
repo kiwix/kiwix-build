a84890af27662cefa14a325fc4ca1fbe0e32a254
Matthieu Gautier <mgautier@kymeria.fr>
2018-08-20 17:27:34 +0200

    WIP use better_wchar_convert

--- appveyor/install_libzim.cmd
+++ appveyor/install_libzim.cmd
@@ -2,6 +2,7 @@ REM ========================================================
 REM Install libzim
 git clone https://github.com/openzim/libzim.git || exit /b 1
 cd libzim
+git checkout better_wchar_convert
 meson.py . build --prefix %EXTRA_DIR% --default-library static --buildtype release || exit /b 1
 cd build
 ninja || exit /b 1
