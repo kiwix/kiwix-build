7963fd464a7fbe8e611727864b5ec596cbe2e53c
Matthieu Gautier <mgautier@kymeria.fr>
2018-10-23 14:18:07 +0200

    [AppVeyor] Update to last version of meson
    Last version of meson (0.48.0) install meson a differently on Windows.
    Now, a `meson.exe` is installed, so, we must launch `meson` instead of `meson.py`.

--- appveyor/install_kiwix-lib.cmd
+++ appveyor/install_kiwix-lib.cmd
@@ -2,7 +2,7 @@ REM ========================================================
 REM Install kiwix-lib
 git clone https://github.com/kiwix/kiwix-lib.git || exit /b 1
 cd kiwix-lib
-meson.py . build --prefix %EXTRA_DIR% --default-library static --buildtype release || exit /b 1
+meson . build --prefix %EXTRA_DIR% --default-library static --buildtype release || exit /b 1
 cd build
 ninja || exit /b 1
 ninja install || exit /b 1
--- appveyor/install_libzim.cmd
+++ appveyor/install_libzim.cmd
@@ -2,7 +2,7 @@ REM ========================================================
 REM Install libzim
 git clone https://github.com/openzim/libzim.git || exit /b 1
 cd libzim
-meson.py . build --prefix %EXTRA_DIR% --default-library static --buildtype release || exit /b 1
+meson . build --prefix %EXTRA_DIR% --default-library static --buildtype release || exit /b 1
 cd build
 ninja || exit /b 1
 ninja install || exit /b 1
