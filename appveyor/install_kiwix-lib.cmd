95ffc8c3b351c30de9bd6b0ed2c31f9fbb22cc8e
Matthieu Gautier <mgautier@kymeria.fr>
2018-11-03 10:39:14 +0100

    TOREVERT use branch win_relpath

--- appveyor/install_kiwix-lib.cmd
+++ appveyor/install_kiwix-lib.cmd
@@ -2,6 +2,7 @@ REM ========================================================
 REM Install kiwix-lib
 git clone https://github.com/kiwix/kiwix-lib.git || exit /b 1
 cd kiwix-lib
+git checkout win_relpath
 meson . build --prefix %EXTRA_DIR% --default-library static --buildtype release || exit /b 1
 cd build
 ninja || exit /b 1
