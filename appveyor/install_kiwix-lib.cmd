f844832c87d58658f9e604c8418b72f3c987e3ec
Matthieu Gautier <mgautier@kymeria.fr>
2019-01-11 11:28:41 +0100

    [Appveyor]Add include directory for mustache in kiwix-lib.

--- appveyor/install_kiwix-lib.cmd
+++ appveyor/install_kiwix-lib.cmd
@@ -2,6 +2,7 @@ REM ========================================================
 REM Install kiwix-lib
 git clone https://github.com/kiwix/kiwix-lib.git || exit /b 1
 cd kiwix-lib
+set CPPFLAGS="-I%EXTRA_DIR%/include"
 meson . build --prefix %EXTRA_DIR% --default-library static --buildtype release || exit /b 1
 cd build
 ninja || exit /b 1
