2970a2d4458b5cd248ff5473a9696101aa3d6237
Matthieu Gautier <mgautier@kymeria.fr>
2019-03-06 08:59:14 +0100

    Update to new version of lzma and get it from kiwix.org server.
    It seems that the downloading of the archive stored on sourceforge
    fails sometime.
    
    Use the archive stored in our server (download.kiwix.org) instead.
    And also move to the last version of lzma.

--- kiwixbuild/dependencies/lzma.py
+++ kiwixbuild/dependencies/lzma.py
@@ -9,9 +9,9 @@ class lzma(Dependency):
     name = 'lzma'
 
     class Source(ReleaseDownload):
-        archive = Remotefile('xz-5.2.3.tar.bz2',
-                             'fd9ca16de1052aac899ad3495ad20dfa906c27b4a5070102a2ec35ca3a4740c1',
-                             'https://tukaani.org/xz/xz-5.2.3.tar.bz2')
+        archive = Remotefile('xz-5.2.4.tar.gz',
+                             'b512f3b726d3b37b6dc4c8570e137b9311e7552e8ccbab4d39d47ce5f4177145'
+                            )
 
     class Builder(MakeBuilder):
         @property
