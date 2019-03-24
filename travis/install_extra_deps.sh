d593a3143d0b78ab61bbc7dddfcc5c4da798e5aa
Rafid Aslam <rafidteam@gmail.com>
2019-03-24 19:39:43 +0700

    Add flake8 check to Travis-CI matrix
    Closes https://github.com/kiwix/kiwix-build/issues/263

--- travis/install_extra_deps.sh
+++ travis/install_extra_deps.sh
@@ -4,11 +4,16 @@ set -e
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]
 then
-  pip3 install pillow
-  pip3 install .
+  if [[ "$TEST_SUITE" == "flake8_check" ]]
+  then
+    pip3 install flake8
+  else
+    pip3 install pillow
+    pip3 install .
 
-  wget https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-mac.zip
-  unzip ninja-mac.zip ninja
+    wget https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-mac.zip
+    unzip ninja-mac.zip ninja
+  fi
 else
   wget https://bootstrap.pypa.io/get-pip.py
   python3.5 get-pip.py --user
@@ -21,6 +26,8 @@ else
   unzip ninja-linux.zip ninja
 fi
 
-mkdir -p $HOME/bin
-cp ninja $HOME/bin
-
+if [[ "$TEST_SUITE" != "flake8_check" ]]
+then
+  mkdir -p $HOME/bin
+  cp ninja $HOME/bin
+fi
