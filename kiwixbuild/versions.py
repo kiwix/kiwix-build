d3b0d76726274043658acb2ddc58ac5e00f3a3b1
Kelson <kelson@kiwix.org>
2019-06-12 15:50:18 +0200

    Revert "update android sdk tools to 26.1.1"
    This reverts commit 51029fe820aebda8a325089e82752942073b092d.

--- kiwixbuild/versions.py
+++ kiwixbuild/versions.py
@@ -50,7 +50,7 @@ base_deps_versions = {
   'gradle' : '5.2',
   'libaria2' : '1.33.1',
   'libmagic' : '5.35',
-  'android-sdk' : 'r26.1.1',
+  'android-sdk' : 'r25.2.3',
   'android-ndk' : 'r13b',
   'qt' : '5.10.1',
   'qtwebengine' : '5.10.1',
