c2c715802d1727143f5bcb7b9c5c9e8c318ce656
Matthieu Gautier <mgautier@kymeria.fr>
2019-08-22 16:07:38 +0200

    New version of libzim and kiwix-lib.

--- kiwixbuild/versions.py
+++ kiwixbuild/versions.py
@@ -1,9 +1,9 @@
 # This file reference all the versions of the depedencies we use in kiwix-build.
 
 main_project_versions = {
-    'kiwix-lib': '6.0.1',
+    'kiwix-lib': '6.0.2',
     'kiwix-tools': '3.0.0',
-    'libzim': '5.0.2',
+    'libzim': '5.1.0',
     'zim-tools': '1.0.4',
     'zimwriterfs': '1.3.5',
     'kiwix-desktop': '2.0-rc1' # Also need to be change in appveyor/package_kiwix-desktop.sh
@@ -24,11 +24,11 @@ main_project_versions = {
 #    - Else, increment the value. If no value was present, see `(was ...)`.
 
 release_versions = {
-    'libzim': None, # Depends of base deps (was 0)
+    'libzim': 0, # Depends of base deps (was 0)
     'kiwix-lib': 0, # Depends of libzim (was 0)
     'kiwix-tools': None, # Depends of kiwix-lib and libzim (was 0)
-    'zim-tools': None, # Depends of libzim (was 1)
-    'zimwriterfs': None, # Depends of libzim (was 1)
+    'zim-tools': 2, # Depends of libzim (was 1)
+    'zimwriterfs': 2, # Depends of libzim (was 1)
     'kiwix-desktop': None # Depends of kiwix-lib and libzim (was 1)
 }
 
