d63684975fcce72d1bad3349df32514ef2cae1f5
Matthieu Gautier <mgautier@kymeria.fr>
2019-09-02 18:13:08 +0200

    Use a build version to publish the windows release of kiwix-desktop.

--- appveyor/package_kiwix-desktop.sh
+++ appveyor/package_kiwix-desktop.sh
@@ -7,23 +7,28 @@ then
   KIWIX_ARCHIVES_DIR=/c/projects/NIGHTLY_KIWIX_ARCHIVES/${NIGHTLY_DATE}
   KIWIX_DIR=kiwix-desktop_windows_x64_$NIGHTLY_DATE
 else
-  KIWIX_DESKTOP_VERSION=2.0-rc1
-  KIWIX_ARCHIVES_DIR=/c/projects/RELEASE_KIWIX_ARCHIVES
-  KIWIX_DIR=kiwix-desktop_windows_x64_${KIWIX_DESKTOP_VERSION}
+  if [ $KIWIX_DESKTOP_RELEASE  -eq 1 ]
+  then
+    KIWIX_ARCHIVES_DIR=/c/projects/RELEASE_KIWIX_ARCHIVES
+    KIWIX_DIR=kiwix-desktop_windows_x64_${KIWIX_DESKTOP_VERSION}
+  fi
 fi
 
-KIWIX_ARCH_NAME=${KIWIX_DIR}.zip
+if [[ "$KIWIX_DIR" ]]
+then
+  KIWIX_ARCH_NAME=${KIWIX_DIR}.zip
 
-mkdir $KIWIX_DIR
-mkdir -p KIWIX_ARCHIVES_DIR
+  mkdir $KIWIX_DIR
+  mkdir -p KIWIX_ARCHIVES_DIR
 
-cp /c/projects/kiwix-build/kiwix-desktop/Release/kiwix-desktop.exe $KIWIX_DIR
+  cp /c/projects/kiwix-build/kiwix-desktop/Release/kiwix-desktop.exe $KIWIX_DIR
 /c/Qt/5.11/msvc2017_64/bin/windeployqt --compiler-runtime $KIWIX_DIR
 
-cp $MINGW64_EXTRA_DIR/aria2c.exe $KIWIX_DIR
-cp $MINGW64_EXTRA_DIR/bin/*.dll $KIWIX_DIR
-cp $MINGW64_EXTRA_DIR/bin/kiwix-serve.exe $KIWIX_DIR
+  cp $MINGW64_EXTRA_DIR/aria2c.exe $KIWIX_DIR
+  cp $MINGW64_EXTRA_DIR/bin/*.dll $KIWIX_DIR
+  cp $MINGW64_EXTRA_DIR/bin/kiwix-serve.exe $KIWIX_DIR
 
-/c/Program\ Files\ \(x86\)/Windows\ Kits/10/bin/x64/signtool.exe sign -f appveyor/kiwix.pfx -p $win_certificate_password -t http://timestamp.verisign.com/scripts/timestamp.dll -d "Kiwix-desktop application" $KIWIX_DIR/kiwix-desktop.exe
+  /c/Program\ Files\ \(x86\)/Windows\ Kits/10/bin/x64/signtool.exe sign -f appveyor/kiwix.pfx -p $win_certificate_password -t http://timestamp.verisign.com/scripts/timestamp.dll -d "Kiwix-desktop application" $KIWIX_DIR/kiwix-desktop.exe
 
-7z a -tzip $KIWIX_ARCHIVES_DIR/$KIWIX_ARCH_NAME $KIWIX_DIR
+  7z a -tzip $KIWIX_ARCHIVES_DIR/$KIWIX_ARCH_NAME $KIWIX_DIR
+fi
