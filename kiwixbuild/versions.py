bffce48861995be85b93fae973865fff3ac59495
Matthieu Gautier <mgautier@kymeria.fr>
2019-09-04 11:15:41 +0200

    New release of zimwriterfs.
    Also build kiwix-lib 6.0.3.

--- kiwixbuild/versions.py
+++ kiwixbuild/versions.py
@@ -5,7 +5,7 @@ main_project_versions = {
     'kiwix-tools': '3.0.1',
     'libzim': '6.0.0',
     'zim-tools': '1.0.5',
-    'zimwriterfs': '1.3.6',
+    'zimwriterfs': '1.3.7',
     'kiwix-desktop': '2.0-rc1' # Also need to be change in appveyor/package_kiwix-desktop.sh
                                # Also change KIWIX_DESKTOP_VERSION and KIWIX_DESKTOP_RELEASE in appveyor.yml
 }
@@ -31,12 +31,12 @@ main_project_versions = {
 # - set KIWIX_DESKTOP_RELEASE to 0
 
 release_versions = {
-    'libzim': 0, # Depends of base deps (was 0)
-    'kiwix-lib': 1, # Depends of libzim (was 0)
+    'libzim': None, # Depends of base deps (was 0)
+    'kiwix-lib': 0, # Depends of libzim (was 0)
     'kiwix-tools': 2, # Depends of kiwix-lib and libzim (was 1)
-    'zim-tools': 0, # Depends of libzim (was 0)
+    'zim-tools': None, # Depends of libzim (was 0)
     'zimwriterfs': 0, # Depends of libzim (was 0)
-    'kiwix-desktop': 2 # Depends of kiwix-lib and libzim (was 1)
+    'kiwix-desktop': 3 # Depends of kiwix-lib and libzim (was 2)
 }
 
 
