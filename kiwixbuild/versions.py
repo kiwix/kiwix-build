29956ee6befbded62960aea1e46759ca4efcba03
Kelson <kelson@kiwix.org>
2020-02-10 14:36:11 +0100

    Release zimwriterfs 1.3.8

--- kiwixbuild/versions.py
+++ kiwixbuild/versions.py
@@ -5,7 +5,7 @@ main_project_versions = {
     'kiwix-tools': '3.0.1', # Also need to change this in appveyor/install_kiwix-tools.cmd
     'libzim': '6.0.2',
     'zim-tools': '1.0.5',
-    'zimwriterfs': '1.3.7',
+    'zimwriterfs': '1.3.8',
     'kiwix-desktop': '2.0-rc4' # Also change KIWIX_DESKTOP_VERSION and KIWIX_DESKTOP_RELEASE in appveyor.yml
                                # You need to do the release process of kiwix-tools and kiwix-desktop in two separated process.
 }
@@ -32,10 +32,10 @@ main_project_versions = {
 
 release_versions = {
     'libzim': None, # Depends of base deps (was 0)
-    'kiwix-lib': 0, # Depends of libzim (was 0)
+    'kiwix-lib': None, # Depends of libzim (was 0)
     'kiwix-tools': None, # Depends of kiwix-lib and libzim (was 7)
     'zim-tools': None, # Depends of libzim (was 7)
-    'zimwriterfs': None, # Depends of libzim (was 1)
+    'zimwriterfs': 0, # Depends of libzim (was 1)
     'kiwix-desktop': None # Depends of kiwix-lib and libzim (was 0)
 }
 
