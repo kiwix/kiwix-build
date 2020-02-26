8cecb1c759b5942e73d9ef989ae9169f45a976c1
Matthieu Gautier <mgautier@kymeria.fr>
2020-02-26 18:32:00 +0100

    Rebuild kiwix-lib (for android)

--- kiwixbuild/versions.py
+++ kiwixbuild/versions.py
@@ -32,7 +32,7 @@ main_project_versions = {
 
 release_versions = {
     'libzim': None, # Depends of base deps (was 0)
-    'kiwix-lib': 0, # Depends of libzim (was 0)
+    'kiwix-lib': 1, # Depends of libzim (was 0)
     'kiwix-tools': None, # Depends of kiwix-lib and libzim (was 0)
     'zim-tools': None, # Depends of libzim (was 7)
     'zimwriterfs': None, # Depends of libzim (was 1)
