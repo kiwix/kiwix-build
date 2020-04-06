25d9e18e3dd0b73887f10e7d1d4fc82350c00328
Veloman Yunkan <veloman.yunkan@gmail.com>
2020-04-07 00:46:23 +0400

    Temporary fix for facebook/zstd#2073

--- kiwixbuild/dependencies/zstd.py
+++ kiwixbuild/dependencies/zstd.py
@@ -14,6 +14,7 @@ class zstd(Dependency):
         archive = Remotefile('zstd-1.4.4.tar.gz',
                              '59ef70ebb757ffe74a7b3fe9c305e2ba3350021a918d168a046c6300aeea9315',
                              'https://github.com/facebook/zstd/releases/download/v1.4.4/zstd-1.4.4.tar.gz')
+        patches = ['zstd_meson.patch']
 
     class Builder(MesonBuilder):
         subsource_dir = 'build/meson'
