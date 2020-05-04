69c33da0bcf1384e9527e7c5461321d933ee07c9
MiguelRocha <miguelrocha@ua.pt>
2020-05-04 15:51:21 +0100

    Now kiwix-build retrives docopt from a commit that has pkgconfig support.

--- kiwixbuild/dependencies/docoptcpp.py
+++ kiwixbuild/dependencies/docoptcpp.py
@@ -1,6 +1,6 @@
 from .base import (
     Dependency,
-    ReleaseDownload,
+    GitClone,
     CMakeBuilder)
 
 from kiwixbuild.utils import Remotefile
@@ -10,10 +10,10 @@ from kiwixbuild.utils import Remotefile
 class docoptcpp(Dependency):
     name = 'docoptcpp'
 
-    class Source(ReleaseDownload):
-        archive = Remotefile('v0.6.2.tar.gz',
-                             'c05542245232420d735c7699098b1ea130e3a92bade473b64baf876cdf098a17',
-                             'https://github.com/docopt/docopt.cpp/archive/v0.6.2.tar.gz')
+    class Source(GitClone):
+        git_remote = "https://github.com/docopt/docopt.cpp.git"
+        git_dir = "docopt.cpp"
+        git_ref = "3dd23e3280f213bacefdf5fcb04857bf52e90917"
 
     class Builder(CMakeBuilder):
         make_install_target = 'install'
