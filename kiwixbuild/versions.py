39ca099248b1c06b83bc7ad75988523b7ac8427e
Matthieu Gautier <mgautier@kymeria.fr>
2020-06-03 15:49:47 +0200

    New version of libzim.
    No need to rebuild other project as there is code change.

--- kiwixbuild/versions.py
+++ kiwixbuild/versions.py
@@ -3,7 +3,7 @@
 main_project_versions = {
     'kiwix-lib': '9.2.2',
     'kiwix-tools': '3.1.1', # Also need to change this in appveyor/install_kiwix-tools.cmd
-    'libzim': '6.1.4',
+    'libzim': '6.1.5',
     'zim-tools': '1.2.1',
     'zimwriterfs': '1.3.10',
     'kiwix-desktop': '2.0.1' # Also change KIWIX_DESKTOP_VERSION and KIWIX_DESKTOP_RELEASE in appveyor.yml
@@ -31,9 +31,9 @@ main_project_versions = {
 # - set KIWIX_DESKTOP_RELEASE to 0
 
 release_versions = {
-    'libzim': None, # Depends of base deps (was 0)
-    'kiwix-lib': 0, # Depends of libzim (was 0)
-    'kiwix-tools': 4, # Depends of kiwix-lib and libzim (was 3)
+    'libzim': 0, # Depends of base deps (was 0)
+    'kiwix-lib': None, # Depends of libzim (was 0)
+    'kiwix-tools': None, # Depends of kiwix-lib and libzim (was 4)
     'zim-tools': None, # Depends of libzim (was 1)
     'zimwriterfs': None, # Depends of libzim (was 2)
     'kiwix-desktop': None # Depends of kiwix-lib and libzim (was 0)
