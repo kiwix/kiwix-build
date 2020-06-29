3de886f805c4291b75778c56e59ab7b8cc7273d7
Kelson <kelson@kiwix.org>
2020-06-29 16:29:36 +0200

    Upgrade libmicrohttpd to 0.9.71

--- kiwixbuild/dependencies/libmicrohttpd.py
+++ kiwixbuild/dependencies/libmicrohttpd.py
@@ -9,9 +9,9 @@ class MicroHttpd(Dependency):
     name = "libmicrohttpd"
 
     class Source(ReleaseDownload):
-        archive = Remotefile('libmicrohttpd-0.9.63.tar.gz',
-                             '37c36f1be177f0e37ef181a645cd3baac1000bd322a01c2eff70f3cc8c91749c',
-                             'https://ftp.gnu.org/gnu/libmicrohttpd/libmicrohttpd-0.9.63.tar.gz')
+        archive = Remotefile('libmicrohttpd-0.9.71.tar.gz',
+                             'e8f445e85faf727b89e9f9590daea4473ae00ead38b237cf1eda55172b89b182',
+                             'https://ftp.gnu.org/gnu/libmicrohttpd/libmicrohttpd-0.9.71.tar.gz')
 
     class Builder(MakeBuilder):
         configure_option = "--disable-https --without-libgcrypt --without-libcurl --disable-doc --disable-examples"
