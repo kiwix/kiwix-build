da62250d837f3aca16e3298b9284ce0192cd17c9
Matthieu Gautier <mgautier@kymeria.fr>
2020-07-01 11:31:51 +0200

    Build kiwix-desktop without console only if we are doing a release.
    `KIWIX_DESKTOP_RELEASE` is not enough to test as it will be not change
    until the next commit. And we want nightly to build with console output.

--- appveyor/install_kiwix-desktop.cmd
+++ appveyor/install_kiwix-desktop.cmd
@@ -3,12 +3,18 @@ REM Install kiwix-desktop
 git clone https://github.com/kiwix/kiwix-desktop || exit /b 1
 cd kiwix-desktop
 echo "Running qmake"
+SET _WITH_CONSOLE=1
 IF %KIWIX_DESKTOP_RELEASE% EQU 1 (
-    C:\Qt\5.12\msvc2017_64\bin\qmake.exe "CONFIG+=static" || exit /b 1
-) else (
-    C:\Qt\5.12\msvc2017_64\bin\qmake.exe "CONFIG+=static console" || exit /b 1
+  IF %APPVEYOR_REPO_TAG%==true (
+    SET _WITH_CONSOLE=0
+  )
 )
-    
+IF %_WITH_CONSOLE% EQU 1 (
+    C:\Qt\5.12\msvc2017_64\bin\qmake.exe "CONFIG+=static console" || exit /b 1
+) else (
+    C:\Qt\5.12\msvc2017_64\bin\qmake.exe "CONFIG+=static" || exit /b 1
+)
+
 echo "Running fix_desktop"
 C:\Python36\Python ..\appveyor\fix_desktop_makefile.py Makefile.Release || exit /b 1
 echo "Running nmake"
