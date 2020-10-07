a6536251f04bb593f765e712a8aea0b331726cef
Matthieu Gautier <mgautier@kymeria.fr>
2020-10-07 17:32:14 +0200

    [WIP] type kiwix.pc

--- appveyor/install_kiwix-lib.cmd
+++ appveyor/install_kiwix-lib.cmd
@@ -8,4 +8,5 @@ meson . build --prefix %EXTRA_DIR% --default-library static --buildtype release 
 cd build
 ninja || exit /b 1
 ninja install || exit /b 1
+type %EXTRA_DIR%\lib\pkgconfig\kiwix.pc
 cd ..\..
