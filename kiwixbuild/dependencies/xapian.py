3d8578ce8ed54053a35fc12a31479b2a1d4d6fd0
Emmanuel Engelhart <kelson@kiwix.org>
2021-03-07 15:00:58 +0100

    Remove outdated Xapian remote patch

--- kiwixbuild/dependencies/xapian.py
+++ kiwixbuild/dependencies/xapian.py
@@ -14,9 +14,6 @@ class Xapian(Dependency):
     class Source(ReleaseDownload):
         archive = Remotefile('xapian-core-1.4.18.tar.xz',
                              '196ddbb4ad10450100f0991a599e4ed944cbad92e4a6fe813be6dce160244b77')
-        patches = [
-            'xapian_remote.patch'
-        ]
 
     class Builder(MakeBuilder):
         configure_option = "--disable-sse --disable-backend-chert --disable-backend-remote --disable-documentation"
