9d905682040451b3215823ee4a96efde32a3c448
Matthieu Gautier <mgautier@kymeria.fr>
2021-04-28 16:21:04 +0200

    Make libzim use the test data dire downloaded with zim-testing-tool.

--- kiwixbuild/dependencies/libzim.py
+++ kiwixbuild/dependencies/libzim.py
@@ -2,7 +2,7 @@ from .base import (
     Dependency,
     GitClone,
     MesonBuilder)
-from kiwixbuild._global import option
+from kiwixbuild._global import option, get_target_step
 
 class Libzim(Dependency):
     name = "libzim"
@@ -20,12 +20,14 @@ class Libzim(Dependency):
         @property
         def configure_option(self):
             platformInfo = self.buildEnv.platformInfo
+            zim_testing_suite = get_target_step('zim-testing-suite', platformInfo.name)
+            config_options = ['-Dtest_data_dir={}'.format(zim_testing_suite.source_path)]
             if platformInfo.build == 'android':
-                return "-DUSE_BUFFER_HEADER=false"
+                config_options.append("-DUSE_BUFFER_HEADER=false")
             if platformInfo.build == 'iOS':
-                return "-Db_bitcode=true"
+                config_options.append("-Db_bitcode=true")
             if platformInfo.name == 'native_mixed' and option('target') == 'libzim':
-                return "-Dstatic-linkage=true"
+                config_options.append("-Dstatic-linkage=true")
             if platformInfo.name == "flatpak":
-                return "--wrap-mode=nodownload"
-            return ""
+                config_options.append("--wrap-mode=nodownload")
+            return " ".join(config_options)
