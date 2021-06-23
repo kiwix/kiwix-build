afd17b2986284333d9eb694542298471a86bc421
Matthieu Gautier <mgautier@kymeria.fr>
2021-06-23 17:27:05 +0200

    Make kiwix-desktop flatpak use last version of Qt (5.15).
    Adapt flatpack_builder to generate the same manifest than PR
    flathub/org.kiwix.desktop#6

--- kiwixbuild/dependencies/kiwix_desktop.py
+++ kiwixbuild/dependencies/kiwix_desktop.py
@@ -16,11 +16,22 @@ class KiwixDesktop(Dependency):
         make_install_target = 'install'
         configure_env = None
 
+        flatpack_build_options = {
+            "env": [
+                "QMAKEPATH=/app/lib"
+            ]
+        }
+
         @property
         def configure_option(self):
             if self.buildEnv.platformInfo.name == 'flatpak':
-                return []
-            options = ["PREFIX={}".format(self.buildEnv.install_dir)]
-            if self.buildEnv.platformInfo.static:
-                options.append('"CONFIG+=static"')
+                options = [
+                    'QMAKE_INCDIR+=/app/include/QtWebEngine',
+                    'QMAKE_INCDIR+=/app/include/QtWebEngineCore',
+                    'QMAKE_INCDIR+=/app/include/QtWebEngineWidgets'
+                ]
+            else:
+                options = ["PREFIX={}".format(self.buildEnv.install_dir)]
+                if self.buildEnv.platformInfo.static:
+                    options.append('"CONFIG+=static"')
             return " ".join(options)
