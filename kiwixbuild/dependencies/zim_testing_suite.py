f1dfa32a8dadc98970542bc71fb60c608a4ec5ce
Matthieu Gautier <mgautier@kymeria.fr>
2021-06-23 14:57:24 +0200

    Use the correct archive for zim-testing-suite 0.3

--- kiwixbuild/dependencies/zim_testing_suite.py
+++ kiwixbuild/dependencies/zim_testing_suite.py
@@ -12,8 +12,8 @@ class ZimTestingSuite(Dependency):
     dont_skip = True
 
     class Source(ReleaseDownload):
-        archive = Remotefile('zim-testing-suite-0.2.tar.gz',
-                             '04a6db258a48a09ebf25fdf5856029f11269190467b46d54e02c26f2236e2b32',
-                             'https://github.com/openzim/zim-testing-suite/releases/download/v0.2/zim-testing-suite-0.2.tar.gz')
+        archive = Remotefile('zim-testing-suite-0.3.tar.gz',
+                             'cd7d1ccc48af3783af9156cb6bf3c18d9a3319a73fdeefe65f0b4cae402d3d66',
+                             'https://github.com/openzim/zim-testing-suite/releases/download/v0.3/zim-testing-suite-0.3.tar.gz')
 
     Builder = NoopBuilder
