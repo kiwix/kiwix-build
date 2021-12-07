276343c6af558973e263b4fd6158b0d054dc6bfb
Matthieu Gautier <mgautier@kymeria.fr>
2021-12-07 15:01:22 +0100

    Add missing dependency `aria2` to "all" dependencies.
    `AllBaseDependencies` is used as a dummy target to build all base
    dependencies in the CI.

--- kiwixbuild/dependencies/all_dependencies.py
+++ kiwixbuild/dependencies/all_dependencies.py
@@ -13,10 +13,12 @@ class AllBaseDependencies(Dependency):
         @classmethod
         def get_dependencies(cls, platformInfo, allDeps):
             base_deps = ['zlib', 'lzma', 'zstd', 'xapian-core', 'pugixml', 'libcurl', 'icu4c', 'mustache', 'libmicrohttpd', 'zim-testing-suite']
-            # zimtools
-            # We do not build zimtools at all on "android" and "iOS"
+            # Add specific dependencies depending of the platform
             if platformInfo.build not in ('android', 'iOS'):
+                # For zimtools
                 base_deps += ['docoptcpp']
+                # For kiwix-desktop
+                base_deps += ['aria2']
                 if platformInfo.build != 'win32':
                     # zimwriterfs
                     base_deps += ['libmagic', 'gumbo']
