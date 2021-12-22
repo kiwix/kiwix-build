ea7fa1f207c12a64574d9a56f4f4108c12e65910
Matthieu Gautier <mgautier@kymeria.fr>
2021-12-22 16:31:53 +0100

    Remove unnecessary aria2 patch
    This patch was to compile libaria2 on android.
    But we don't compile it for android since a long time.
    As it doesn't apply anymore, let's remove it.

--- kiwixbuild/dependencies/aria2.py
+++ kiwixbuild/dependencies/aria2.py
@@ -13,7 +13,6 @@ class Aria2(Dependency):
         archive = Remotefile('aria2-1.36.0.tar.xz',
                              '58d1e7608c12404f0229a3d9a4953d0d00c18040504498b483305bcb3de907a5',
                              'https://github.com/aria2/aria2/releases/download/release-1.36.0/aria2-1.36.0.tar.xz')
-        patches = ["libaria2_android.patch"]
 
         def _post_prepare_script(self, context):
             context.try_skip(self.extract_path)
