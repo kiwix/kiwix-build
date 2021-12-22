612efaad7c1aa99750f8dd22150c3735f0090ce0
Matthieu Gautier <mgautier@kymeria.fr>
2021-12-22 16:32:18 +0100

    Update aria2c version on windows and appimage

--- appveyor/install_aria2.cmd
+++ appveyor/install_aria2.cmd
@@ -1,4 +1,4 @@
 REM ========================================================
 REM Install aria2
-curl -fsSL -O https://github.com/aria2/aria2/releases/download/release-1.34.0/aria2-1.34.0-win-64bit-build1.zip || exit /b 1
-7z e aria2-1.34.0-win-64bit-build1.zip -o%EXTRA_DIR% aria2-1.34.0-win-64bit-build1\aria2c.exe || exit /b 1
+curl -fsSL -O https://github.com/aria2/aria2/releases/download/release-1.36.0/aria2-1.36.0-win-64bit-build1.zip || exit /b 1
+7z e aria2-1.36.0-win-64bit-build1.zip -o%EXTRA_DIR% aria2-1.36.0-win-64bit-build1\aria2c.exe || exit /b 1
