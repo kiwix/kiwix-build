4ee28541cd621c3fcdfd694fbf031f85cd55e6ca
Matthieu Gautier <mgautier@kymeria.fr>
2021-12-22 15:36:16 +0100

    Remove unnecessary zstd patch.

--- kiwixbuild/dependencies/zstd.py
+++ kiwixbuild/dependencies/zstd.py
@@ -14,7 +14,6 @@ class zstd(Dependency):
         archive = Remotefile('zstd-1.5.1.tar.gz',
                              'e28b2f2ed5710ea0d3a1ecac3f6a947a016b972b9dd30242369010e5f53d7002',
                              'https://github.com/facebook/zstd/releases/download/v1.5.1/zstd-1.5.1.tar.gz')
-        patches = ['zstd_meson.patch']
 
     class Builder(MesonBuilder):
         subsource_dir = 'build/meson'
