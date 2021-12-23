462538f4a565ba6c820774a053d432a08a8f7f18
Matthieu Gautier <mgautier@kymeria.fr>
2021-12-23 19:01:15 +0100

    Do not try to cross compile aria2.
    Aria2 is a special case. We actually don't really need to compile it.
    kiwix-desktop through libkiwix use the aria2c binary but we always
    use (package) the system binary or a prebuild release.
    
    And we don't cross compile kiwix-desktop to windows, so we don't need
    aria2c neither.

--- kiwixbuild/dependencies/all_dependencies.py
+++ kiwixbuild/dependencies/all_dependencies.py
@@ -17,10 +17,11 @@ class AllBaseDependencies(Dependency):
             if platformInfo.build not in ('android', 'iOS'):
                 # For zimtools
                 base_deps += ['docoptcpp']
-                # For kiwix-desktop
-                base_deps += ['aria2']
                 if platformInfo.build != 'win32':
                     # zimwriterfs
                     base_deps += ['libmagic', 'gumbo']
-
+                if platformInfo.build == 'native' and neutralEnv('distname') != 'Darwin':
+                    # We compile kiwix-desktop only on native and not on `Darwin`
+                    # So we need aria2 only there
+                    base_deps += ['aria2']
             return base_deps
