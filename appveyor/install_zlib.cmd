5a6af5f5ab39decc6878c53c7817cb4d26d882dc
Matthieu Gautier <mgautier@kymeria.fr>
2022-04-11 17:47:17 +0200

    Use the last version of zlib on Windows.
    The `zlib-1.2.12.meson.zip` is the composition of the official
    `zlib.1.2.12.zip` archive and the meson wrabdb "patch"
    https://wrapdb.mesonbuild.com/v2/zlib_1.2.12-1/get_patch as described
    in https://wrapdb.mesonbuild.com/v2/zlib_1.2.12-1/zlib.wrap

--- appveyor/install_zlib.cmd
+++ appveyor/install_zlib.cmd
@@ -1,8 +1,8 @@
 REM ========================================================
 REM Install zlib
-curl -fsSL -O http://public.kymeria.fr/KIWIX/windows/zlib-1.2.11.meson.zip || exit /b 1
-7z x zlib-1.2.11.meson.zip || exit /b 1
-cd zlib-1.2.11
+curl -fsSL -O http://mirror.download.kiwix.org/dev/zlib-1.2.12.meson.zip || exit /b 1
+7z x zlib-1.2.12.meson.zip || exit /b 1
+cd zlib-1.2.12
 meson . build --prefix %EXTRA_DIR% --default-library static --buildtype release || exit /b 1
 cd build
 ninja || exit /b 1
