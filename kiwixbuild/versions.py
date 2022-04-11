c5662d4d7bb41a05f5364e0982e459e3b9b2002c
Matthieu Gautier <mgautier@kymeria.fr>
2022-04-11 17:44:08 +0200

    New version of libkiwix 10.1.1
    As the base dependencies changed, we also need to rebuild all our projects.

--- kiwixbuild/versions.py
+++ kiwixbuild/versions.py
@@ -2,7 +2,7 @@
 
 main_project_versions = {
     'libzim': '7.2.0',
-    'libkiwix': '10.1.0',
+    'libkiwix': '10.1.1',
     'kiwix-tools': '3.2.0',
     'zim-tools': '3.1.0',
     'kiwix-desktop': '2.2.1' # Also change KIWIX_DESKTOP_VERSION and KIWIX_DESKTOP_RELEASE in appveyor.yml
@@ -29,11 +29,11 @@ main_project_versions = {
 # - set KIWIX_DESKTOP_RELEASE to 0
 
 release_versions = {
-    'libzim': None, # Depends of base deps (was 0)
-    'libkiwix': None, # Depends of libzim (was 0)
-    'kiwix-tools': 2, # Depends of libkiwix and libzim (was 1)
-    'zim-tools': None, # Depends of libzim (was 0)
-    'kiwix-desktop': None # Depends of libkiwix and libzim (was 1)
+    'libzim': 1, # Depends of base deps (was 0)
+    'libkiwix': 0, # Depends of libzim (was 0)
+    'kiwix-tools': 3, # Depends of libkiwix and libzim (was 2)
+    'zim-tools': 1, # Depends of libzim (was 0)
+    'kiwix-desktop': 2 # Depends of libkiwix and libzim (was 1)
 }
 
 
