5d967b3b262fb8cf05d243ba07c5bb064ca30abf
Matthieu Gautier <mgautier@kymeria.fr>
2022-04-11 18:13:53 +0200

    Don't apply xapians patch on windows
    The patch doesn't exist...

--- appveyor/install_xapian.cmd
+++ appveyor/install_xapian.cmd
@@ -2,7 +2,6 @@ REM ========================================================
 REM Install xapian
 curl -fsSL -O http://mirror.download.kiwix.org/dev/xapian-core-1.4.18.zip || exit /b 1
 7z x xapian-core-1.4.18.zip || exit /b 1
-%MINGW64_RUN% "cd /c/Projects/kiwix-build/xapian-core-1.4.18 && ../appveyor/apply_patch.sh xapian_remote.patch" || exit /b 1
 cd xapian-core-1.4.18
 mkdir build
 cd build
