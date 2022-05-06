32a6e5b58828ef9785eaf9bcc2c8543d5ba84662
Matthieu Gautier <mgautier@kymeria.fr>
2022-05-06 14:51:16 +0200

    New version of libzim 7.2.1 and zim-tools 3.1.1

--- kiwixbuild/versions.py
+++ kiwixbuild/versions.py
@@ -1,10 +1,10 @@
 # This file reference all the versions of the depedencies we use in kiwix-build.
 
 main_project_versions = {
-    'libzim': '7.2.0',
+    'libzim': '7.2.1',
     'libkiwix': '10.1.1',
     'kiwix-tools': '3.2.0',
-    'zim-tools': '3.1.0',
+    'zim-tools': '3.1.1',
     'kiwix-desktop': '2.2.1' # Also change KIWIX_DESKTOP_VERSION and KIWIX_DESKTOP_RELEASE in appveyor.yml
 }
 
@@ -29,11 +29,11 @@ main_project_versions = {
 # - set KIWIX_DESKTOP_RELEASE to 0
 
 release_versions = {
-    'libzim': 1, # Depends of base deps (was 0)
-    'libkiwix': 0, # Depends of libzim (was 0)
-    'kiwix-tools': 3, # Depends of libkiwix and libzim (was 2)
-    'zim-tools': 1, # Depends of libzim (was 0)
-    'kiwix-desktop': 2 # Depends of libkiwix and libzim (was 1)
+    'libzim': 0, # Depends of base deps (was 0)
+    'libkiwix': 1, # Depends of libzim (was 0)
+    'kiwix-tools': 4, # Depends of libkiwix and libzim (was 3)
+    'zim-tools': 0, # Depends of libzim (was 0)
+    'kiwix-desktop': 3 # Depends of libkiwix and libzim (was 2)
 }
 
 
