897e7f292c7a655592a29f8dbef85ccb730729cc
Matthieu Gautier <mgautier@kymeria.fr>
2023-05-16 10:35:47 +0200

    Switch (again) to new arm cross-compilation toolchain.
    This introduce a difference between armv6 and armv8.

--- kiwixbuild/dependencies/armhf.py
+++ kiwixbuild/dependencies/armhf.py
@@ -1,27 +1,35 @@
 from .base import Dependency, ReleaseDownload, NoopBuilder
 from kiwixbuild.utils import Remotefile
 
+# The arm toolchains
+# This is based on toolchains published here : https://github.com/tttapa/docker-arm-cross-toolchain
 
-base_url = 'https://master.dl.sourceforge.net/project/raspberry-pi-cross-compilers/'
+base_url = "https://github.com/tttapa/docker-arm-cross-toolchain/releases/download/0.1.0/"
 
-# This is Gcc 10.3.0 and Raspberry Pi 2 and 3 only !
-armhf_base_url = base_url + 'Raspberry%20Pi%20GCC%20Cross-Compiler%20Toolchains/Stretch/GCC%2010.3.0/Raspberry%20Pi%202%2C%203/'
-
-# This is Gcc 10.3.0 and ALL rapsberry Pi arch64
-aarch_base_url = base_url + 'Bonus%20Raspberry%20Pi%20GCC%2064-Bit%20Toolchains/Raspberry%20Pi%20GCC%2064-Bit%20Cross-Compiler%20Toolchains/Stretch/GCC%206.3.0/'
-
-class armhf_toolchain(Dependency):
+class armv6_toolchain(Dependency):
     dont_skip = True
     neutral = True
-    name = 'armhf'
+    name = 'armv6'
 
     class Source(ReleaseDownload):
-        archive = Remotefile('cross-gcc-10.3.0-pi_2-3.tar.gz',
-                             '6aef31703fb7bfd63065dda7fb525f1f86a0509c4358c57631a51025805278b3',
-                             armhf_base_url + 'cross-gcc-10.3.0-pi_2-3.tar.gz')
+        archive = Remotefile('x-tools-armv6-rpi-linux-gnueabihf.tar.xz',
+                             '4c371c4c5b55ebd1f3d7dd26b14703632d9ba47423f901bcd9303d83ad444434',
+                             base_url + 'x-tools-armv6-rpi-linux-gnueabihf.tar.xz')
 
     Builder = NoopBuilder
 
+
+class armv8_toolchain(Dependency):
+    dont_skip = True
+    neutral = True
+    name = 'armv8'
+
+    class Source(ReleaseDownload):
+        archive = Remotefile('x-tools-armv8-rpi-linux-gnueabihf.tar.xz',
+                             'cc28f5c3f6a3e7d9985f98779c4e72224b4eb5a7e4dc2bcdefd90cb241fb94a5',
+                             base_url + 'x-tools-armv8-rpi3-linux-gnueabihf.tar.xz')
+
+    Builder = NoopBuilder
 
 class aarch64_toolchain(Dependency):
     dont_skip = True
@@ -29,8 +37,8 @@ class aarch64_toolchain(Dependency):
     name = "aarch64"
 
     class Source(ReleaseDownload):
-        archive = Remotefile('cross-gcc-6.3.0-pi_64.tar.gz',
-                             '1b048bb8886ad63d21797cd9129fc37b9ea0dfaac7e3c36f888aa16fbec1d320',
-                             aarch_base_url + 'cross-gcc-6.3.0-pi_64.tar.gz')
+        archive = Remotefile('x-tools-aarch64-rpi3-linux-gnu.tar.xz',
+                             '8be81d3fc47b1b280bf003646d2b623477badec4ec931944131bf412317b6332',
+                             base_url + 'x-tools-aarch64-rpi3-linux-gnu.tar.xz')
 
     Builder = NoopBuilder
