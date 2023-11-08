0a02353f281067858b4de3b3e3f6ef8693e9771e
Matthieu Gautier <mgautier@kymeria.fr>
2023-11-08 10:43:04 +0100

    Do not publish projects deps if we are in a branch.
    Still publish the deps in the dep_previews directory in case we
    would like to test it project's CI.

--- .github/scripts/compile_all_deps.py
+++ .github/scripts/compile_all_deps.py
@@ -6,13 +6,17 @@ from common import (
     run_kiwix_build,
     make_deps_archive,
     upload,
-    OS_NAME,
     PLATFORM_TARGET,
+    DEV_BRANCH,
 )
 from build_definition import select_build_targets, DEPS
 
 for target in select_build_targets(DEPS):
     run_kiwix_build(target, platform=PLATFORM_TARGET, build_deps_only=True)
     archive_file = make_deps_archive(target=target)
-    upload(archive_file, "ci@tmp.kiwix.org:30022", "/data/tmp/ci")
+    if DEV_BRANCH:
+        destination = "/data/tmp/ci/dev_preview/" + DEV_BRANCH
+    else:
+        destination = "/data/tmp/ci"
+    upload(archive_file, "ci@tmp.kiwix.org:30022", destination)
     os.remove(str(archive_file))
