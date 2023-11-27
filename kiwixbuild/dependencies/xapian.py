de2338510ef8af373758e0ae028404099f1c7315
Matthieu Gautier <mgautier@kymeria.fr>
2023-11-27 16:15:41 +0100

    Build xapian using meson build system.

--- kiwixbuild/dependencies/xapian.py
+++ kiwixbuild/dependencies/xapian.py
@@ -1,7 +1,7 @@
 from .base import (
     Dependency,
     ReleaseDownload,
-    MakeBuilder
+    MakeBuilder, MesonBuilder
 )
 
 from kiwixbuild.utils import Remotefile
@@ -12,18 +12,28 @@ class Xapian(Dependency):
     name = "xapian-core"
 
     class Source(ReleaseDownload):
-        archive = Remotefile('xapian-core-1.4.23.tar.xz',
+        src_archive = Remotefile('xapian-core-1.4.23.tar.xz',
                              '30d3518172084f310dab86d262b512718a7f9a13635aaa1a188e61dc26b2288c')
+        meson_patch = Remotefile('xapian-core-1.4.23-1_patch.zip',
+                                 '',
+                                 'https://public.kymeria.fr/KIWIX/xapian-core-1.4.23-1_patch.zip')
+        archives = [src_archive, meson_patch]
 
-    class Builder(MakeBuilder):
+#    class Builder(MakeBuilder):
+#        configure_options = [
+#            "--disable-sse",
+#            "--disable-backend-chert",
+#            "--disable-backend-remote",
+#            "--disable-documentation"]
+#        configure_env = {'_format_LDFLAGS': "{env.LDFLAGS} -L{buildEnv.install_dir}/{buildEnv.libprefix}",
+#                         '_format_CXXFLAGS': "{env.CXXFLAGS} -I{buildEnv.install_dir}/include"}
+
+    class Builder(MesonBuilder):
         configure_options = [
-            "--disable-sse",
-            "--disable-backend-chert",
-            "--disable-backend-remote",
-            "--disable-documentation"]
-        configure_env = {'_format_LDFLAGS': "{env.LDFLAGS} -L{buildEnv.install_dir}/{buildEnv.libprefix}",
-                         '_format_CXXFLAGS': "{env.CXXFLAGS} -O3 -I{buildEnv.install_dir}/include"}
-
+            "-Denable-sse=false",
+            "-Denable-backend-chert=false",
+            "-Denable-backend-remote=false"
+        ]
 
         @classmethod
         def get_dependencies(cls, platformInfo, allDeps):
