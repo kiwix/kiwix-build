f9b890c58df1ff5aeb4f9e6d568809c53415a346
Matthieu Gautier <mgautier@kymeria.fr>
2023-11-27 16:15:41 +0100

    Build xapian using meson build system.

--- kiwixbuild/dependencies/xapian.py
+++ kiwixbuild/dependencies/xapian.py
@@ -1,4 +1,8 @@
-from .base import Dependency, ReleaseDownload, MakeBuilder
+from .base import (
+    Dependency,
+    ReleaseDownload,
+    MakeBuilder, MesonBuilder
+)
 
 from kiwixbuild.utils import Remotefile
 from kiwixbuild._global import neutralEnv
@@ -8,22 +12,32 @@ class Xapian(Dependency):
     name = "xapian-core"
 
     class Source(ReleaseDownload):
-        archive = Remotefile(
+        src_archive = Remotefile(
             "xapian-core-1.4.23.tar.xz",
-            "30d3518172084f310dab86d262b512718a7f9a13635aaa1a188e61dc26b2288c",
+            "30d3518172084f310dab86d262b512718a7f9a13635aaa1a188e61dc26b2288c"
         )
+        meson_patch = Remotefile(
+            "xapian-core-1.4.23-1_patch.zip",
+            "",
+            "https://public.kymeria.fr/KIWIX/xapian-core-1.4.23-1_patch.zip"
+        )
+        archives = [src_archive, meson_patch]
 
-    class Builder(MakeBuilder):
+#    class Builder(MakeBuilder):
+#        configure_options = [
+#            "--disable-sse",
+#            "--disable-backend-chert",
+#            "--disable-backend-remote",
+#            "--disable-documentation"]
+#        configure_env = {'_format_LDFLAGS': "{env.LDFLAGS} -L{buildEnv.install_dir}/{buildEnv.libprefix}",
+#                         '_format_CXXFLAGS': "{env.CXXFLAGS} -I{buildEnv.install_dir}/include"}
+
+    class Builder(MesonBuilder):
         configure_options = [
-            "--disable-sse",
-            "--disable-backend-chert",
-            "--disable-backend-remote",
-            "--disable-documentation",
+            "-Denable-sse=false",
+            "-Denable-backend-chert=false",
+            "-Denable-backend-remote=false"
         ]
-        configure_env = {
-            "_format_LDFLAGS": "{env.LDFLAGS} -L{buildEnv.install_dir}/{buildEnv.libprefix}",
-            "_format_CXXFLAGS": "{env.CXXFLAGS} -O3 -I{buildEnv.install_dir}/include",
-        }
 
         @classmethod
         def get_dependencies(cls, configInfo, allDeps):
