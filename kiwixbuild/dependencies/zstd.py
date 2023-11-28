629d9cf96c1dbddf3fd22b94f41f2c35973c7056
Matthieu Gautier <mgautier@kymeria.fr>
2023-11-28 11:20:18 +0100

    Update zstd

--- kiwixbuild/dependencies/zstd.py
+++ kiwixbuild/dependencies/zstd.py
@@ -11,9 +11,9 @@ class zstd(Dependency):
     name = 'zstd'
 
     class Source(ReleaseDownload):
-        archive = Remotefile('zstd-1.5.2.tar.gz',
-                             'f7de13462f7a82c29ab865820149e778cbfe01087b3a55b5332707abf9db4a6e',
-                             'https://github.com/facebook/zstd/archive/refs/tags/v1.5.2.tar.gz')
+        archive = Remotefile('zstd-1.5.5.tar.gz',
+                             '',
+                             'https://github.com/facebook/zstd/archive/refs/tags/v1.5.5.tar.gz')
 
     class Builder(MesonBuilder):
         subsource_dir = 'build/meson'
