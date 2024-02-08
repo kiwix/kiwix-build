d4acd108caf60fcfc816db19bfb8c735cd3d3fa5
Matthieu Gautier <mgautier@kymeria.fr>
2024-02-08 16:12:13 +0100

    Bump base_deps_meta_version.

--- kiwixbuild/versions.py
+++ kiwixbuild/versions.py
@@ -39,7 +39,7 @@ release_versions = {
 
 # This is the "version" of the whole base_deps_versions dict.
 # Change this when you change base_deps_versions.
-base_deps_meta_version = "96"
+base_deps_meta_version = "97"
 
 base_deps_versions = {
     "zlib": "1.2.12",
