06512650fa522a5813a2feaf41545b5ee4dc3ee5
Emmanuel Engelhart <kelson@kiwix.org>
2024-02-25 15:17:43 +0100

    Release libkiwix 13.1.0

--- kiwixbuild/versions.py
+++ kiwixbuild/versions.py
@@ -2,7 +2,7 @@
 
 main_project_versions = {
     "libzim": "9.1.0",
-    "libkiwix": "13.0.0",
+    "libkiwix": "13.1.0",
     "kiwix-tools": "3.6.0",
     "zim-tools": "3.4.0",
     "kiwix-desktop": "2.3.1",  # Also change KIWIX_DESKTOP_VERSION and KIWIX_DESKTOP_RELEASE in appveyor.yml
@@ -30,9 +30,9 @@ main_project_versions = {
 
 release_versions = {
     "libzim": None,  # Depends of base deps (was 0)
-    "libkiwix": None,  # Depends of libzim (was 1)
+    "libkiwix": 0,  # Depends of libzim (was None)
     "kiwix-tools": None,  # Depends of libkiwix and libzim (was 0)
-    "zim-tools": 0,  # Depends of libzim (was None)
+    "zim-tools": None,  # Depends of libzim (was 0)
     "kiwix-desktop": None,  # Depends of libkiwix and libzim (was 4)
 }
 
