24404914a1b38653147bf0220169de31ad23669c
Matthieu Gautier <mgautier@kymeria.fr>
2024-06-21 10:52:37 +0200

    Move to xapian 1.4.23
    Zip is just official tar.xz repackaged in zip. (Same content)

--- appveyor/install_xapian.cmd
+++ appveyor/install_xapian.cmd
@@ -1,9 +1,9 @@
 REM ========================================================
 REM Install xapian
-curl -fsSL -O http://mirror.download.kiwix.org/dev/kiwix-build/xapian-core-1.4.18.zip || exit /b 1
-7z x xapian-core-1.4.18.zip || exit /b 1
-cd xapian-core-1.4.18
+curl -fsSL -O https://public.kymeria.fr/KIWIX/xapian-core-1.4.23.zip || exit /b 1
+7z x xapian-core-1.4.23.zip || exit /b 1
+cd xapian-core-1.4.23
 mkdir build
 cd build
-%MINGW64_RUN% "cd /c/Projects/kiwix-build/xapian-core-1.4.18/build && /c/Projects/kiwix-build/appveyor/build_xapian.sh" || exit /b 1
+%MINGW64_RUN% "cd /c/Projects/kiwix-build/xapian-core-1.4.23/build && /c/Projects/kiwix-build/appveyor/build_xapian.sh" || exit /b 1
 cd ..\..
