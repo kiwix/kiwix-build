5083ee57c2e712f2358b86fd7f021f90fa64587a
Matthieu Gautier <mgautier@kymeria.fr>
2024-06-21 11:39:55 +0200

    No test_data_dir for libzim

--- appveyor/install_libzim.cmd
+++ appveyor/install_libzim.cmd
@@ -2,7 +2,7 @@ REM ========================================================
 REM Install libzim
 git clone https://github.com/openzim/libzim.git || exit /b 1
 cd libzim
-meson . build --prefix %EXTRA_DIR% --default-library static --buildtype release || exit /b 1
+meson . build --prefix %EXTRA_DIR% --default-library static --buildtype release -Dtest_data_dir=none || exit /b 1
 cd build
 ninja || exit /b 1
 ninja test || exit /b 1
