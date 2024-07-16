e5a6e352e53ece9b3c7a855577cea4a161c5a355
Matthieu Gautier <mgautier@kymeria.fr>
2024-07-16 15:35:47 +0200

    WIP Use fix_locate branch of libzim

--- kiwixbuild/dependencies/libzim.py
+++ kiwixbuild/dependencies/libzim.py
@@ -9,6 +9,7 @@ class Libzim(Dependency):
     class Source(GitClone):
         git_remote = "https://github.com/openzim/libzim.git"
         git_dir = "libzim"
+        git_ref = "fix_locate"
 
     class Builder(MesonBuilder):
         test_options = ["-t", "8"]
