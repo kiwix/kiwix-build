942dba4d9c6d9ca447afc8ce4f3154f070fe6407
Matthieu Gautier <mgautier@kymeria.fr>
2024-07-22 12:15:53 +0200

    Use git source for xapian.
    We have move the meson build system (and various fixes) in a git
    repository.

--- kiwixbuild/dependencies/xapian.py
+++ kiwixbuild/dependencies/xapian.py
@@ -1,8 +1,4 @@
-from .base import (
-    Dependency,
-    ReleaseDownload,
-    MakeBuilder, MesonBuilder
-)
+from .base import Dependency, GitClone, ReleaseDownload, MakeBuilder, MesonBuilder
 
 from kiwixbuild.utils import Remotefile
 from kiwixbuild._global import neutralEnv
@@ -11,26 +7,30 @@ from kiwixbuild._global import neutralEnv
 class Xapian(Dependency):
     name = "xapian-core"
 
-    class Source(ReleaseDownload):
-        src_archive = Remotefile(
-            "xapian-core-1.4.23.tar.xz",
-            "30d3518172084f310dab86d262b512718a7f9a13635aaa1a188e61dc26b2288c"
-        )
-        meson_patch = Remotefile(
-            "xapian-core-1.4.23-1_patch.zip",
-            "",
-            "https://public.kymeria.fr/KIWIX/xapian-core-1.4.23-1_patch.zip"
-        )
-        archives = [src_archive, meson_patch]
+    class Source(GitClone):
+        git_remote = "https://github.com/openzim/xapian-meson.git"
+        git_dir = "xapian-core"
 
-#    class Builder(MakeBuilder):
-#        configure_options = [
-#            "--disable-sse",
-#            "--disable-backend-chert",
-#            "--disable-backend-remote",
-#            "--disable-documentation"]
-#        configure_env = {'_format_LDFLAGS': "{env.LDFLAGS} -L{buildEnv.install_dir}/{buildEnv.libprefix}",
-#                         '_format_CXXFLAGS': "{env.CXXFLAGS} -I{buildEnv.install_dir}/include"}
+    # class Source(ReleaseDownload):
+    #    src_archive = Remotefile(
+    #        "xapian-core-1.4.23.tar.xz",
+    #        "30d3518172084f310dab86d262b512718a7f9a13635aaa1a188e61dc26b2288c"
+    #    )
+    #    meson_patch = Remotefile(
+    #        "xapian-core-1.4.23-1_patch.zip",
+    #        "",
+    #        "https://public.kymeria.fr/KIWIX/xapian-core-1.4.23-1_patch.zip"
+    #    )
+    #    archives = [src_archive, meson_patch]
+
+    #    class Builder(MakeBuilder):
+    #        configure_options = [
+    #            "--disable-sse",
+    #            "--disable-backend-chert",
+    #            "--disable-backend-remote",
+    #            "--disable-documentation"]
+    #        configure_env = {'_format_LDFLAGS': "{env.LDFLAGS} -L{buildEnv.install_dir}/{buildEnv.libprefix}",
+    #                         '_format_CXXFLAGS': "{env.CXXFLAGS} -I{buildEnv.install_dir}/include"}
 
     class Builder(MesonBuilder):
         configure_options = [
@@ -38,6 +38,7 @@ class Xapian(Dependency):
             "-Denable-backend-chert=false",
             "-Denable-backend-remote=false"
         ]
+        subsource_dir = "xapian-core"
 
         @classmethod
         def get_dependencies(cls, configInfo, allDeps):
