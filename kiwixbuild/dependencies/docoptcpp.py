93d3fdd14eca5f6b8258f2dbba0c4753181c4514
Matthieu Gautier <mgautier@kymeria.fr>
2024-08-21 17:27:54 +0200

    Build docoptcpp using Meson

--- kiwixbuild/dependencies/docoptcpp.py
+++ kiwixbuild/dependencies/docoptcpp.py
@@ -1,4 +1,4 @@
-from .base import Dependency, GitClone, CMakeBuilder
+from .base import Dependency, ReleaseDownload, MesonBuilder
 
 from kiwixbuild.utils import Remotefile
 
@@ -6,11 +6,21 @@ from kiwixbuild.utils import Remotefile
 class docoptcpp(Dependency):
     name = "docoptcpp"
 
-    class Source(GitClone):
-        git_remote = "https://github.com/docopt/docopt.cpp.git"
-        git_dir = "docopt.cpp"
-        force_full_clone = True
-        git_ref = "3dd23e3280f213bacefdf5fcb04857bf52e90917"
+    class Source(ReleaseDownload):
+        name = "docoptcpp"
+        src_archive = Remotefile(
+            "v0.6.3.tar.gz",
+            "28af5a0c482c6d508d22b14d588a3b0bd9ff97135f99c2814a5aa3cbff1d6632",
+            "https://github.com/docopt/docopt.cpp/archive/v0.6.3.tar.gz",
+        )
 
-    class Builder(CMakeBuilder):
-        make_install_targets = ["install"]
+        meson_archive = Remotefile(
+            "docopt_0.6.3-3_patch.zip",
+            "1f641187f9d3f35b0a5ebd2011876ef8e9b04b69b7b163095dd7dfa16219ad01",
+            "https://wrapdb.mesonbuild.com/v2/docopt_0.6.3-3/get_patch",
+        )
+
+        archives = [src_archive, meson_archive]
+        patches = ["docopt_meson_install_pkgconfig.patch"]
+
+    Builder = MesonBuilder
