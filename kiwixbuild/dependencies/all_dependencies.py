d8b4d24d7f86f5dc46c363924b6a4deed8327111
Matthieu Gautier <mgautier@kymeria.fr>
2024-08-23 18:07:50 +0200

    Add more dependencies is base dependencies on Windows

--- kiwixbuild/dependencies/all_dependencies.py
+++ kiwixbuild/dependencies/all_dependencies.py
@@ -13,32 +13,50 @@ class AllBaseDependencies(Dependency):
     class Builder(NoopBuilder):
         @classmethod
         def get_dependencies(cls, configInfo, allDeps):
-            if neutralEnv("distname") == "Windows":
-                return ["zlib", "zstd", "icu4c", "zim-testing-suite", "xapian-core"]
             if configInfo.build == "wasm" or environ.get("OS_NAME") == "manylinux":
                 return ["zlib", "lzma", "zstd", "icu4c", "xapian-core"]
 
-            base_deps = [
-                "zlib",
-                "lzma",
-                "zstd",
-                "xapian-core",
-                "pugixml",
-                "libcurl",
-                "icu4c",
-                "mustache",
-                "libmicrohttpd",
-                "zim-testing-suite",
-            ]
-            # Add specific dependencies depending of the config
-            if configInfo.build not in ("android", "iOS"):
-                # For zimtools
-                base_deps += ["docoptcpp"]
-                if configInfo.build != "win32":
-                    # zimwriterfs
-                    base_deps += ["libmagic", "gumbo"]
-                if configInfo.build == "native" and neutralEnv("distname") != "Darwin":
-                    # We compile kiwix-desktop only on native and not on `Darwin`
-                    # So we need aria2 only there
-                    base_deps += ["aria2"]
+            if neutralEnv("distname") == "Windows":
+                base_deps = [
+                    "zlib",
+                    "zstd",
+                    "xapian-core",
+                    "zim-testing-suite",
+                    "icu4c",
+                ]
+
+                if not configInfo.name.endswith("_dyn"):
+                    base_deps += [
+                        "pugixml",
+                        "libcurl",
+                        "mustache",
+                        "libmicrohttpd",
+                    ]
+            else:
+                base_deps = [
+                    "zlib",
+                    "lzma",
+                    "zstd",
+                    "xapian-core",
+                    "pugixml",
+                    "libcurl",
+                    "icu4c",
+                    "mustache",
+                    "libmicrohttpd",
+                    "zim-testing-suite",
+                ]
+                # Add specific dependencies depending of the config
+                if configInfo.build not in ("android", "iOS"):
+                    # For zimtools
+                    base_deps += ["docoptcpp"]
+                    if configInfo.build != "win32":
+                        # zimwriterfs
+                        base_deps += ["libmagic", "gumbo"]
+                    if (
+                        configInfo.build == "native"
+                        and neutralEnv("distname") != "Darwin"
+                    ):
+                        # We compile kiwix-desktop only on native and not on `Darwin`
+                        # So we need aria2 only there
+                        base_deps += ["aria2"]
             return base_deps
