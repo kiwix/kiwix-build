a7f42fcc99ed512da6a7e8c01fa6ee1c16bc889a
Matthieu Gautier <mgautier@kymeria.fr>
2024-08-26 11:04:50 +0200

    Compile zim-tools dependencies on Windows github CI.

--- kiwixbuild/dependencies/zim_tools.py
+++ kiwixbuild/dependencies/zim_tools.py
@@ -1,4 +1,5 @@
 from .base import Dependency, GitClone, MesonBuilder
+from kiwixbuild._global import neutralEnv
 
 
 class ZimTools(Dependency):
@@ -13,14 +14,17 @@ class ZimTools(Dependency):
         @classmethod
         def get_dependencies(cls, configInfo, allDeps):
             base_deps = ["libzim", "docoptcpp", "mustache"]
-            if configInfo.build != "win32":
+            if configInfo.build != "win32" and neutralEnv("distname") != "Windows":
                 base_deps += ["libmagic", "gumbo"]
             return base_deps
 
         @property
         def configure_options(self):
             # We don't build zimwriterfs on win32, and so we don't have magic
-            if self.buildEnv.configInfo.build != "win32":
+            if (
+                self.buildEnv.configInfo.build != "win32"
+                and neutralEnv("distname") != "Windows"
+            ):
                 yield f"-Dmagic-install-prefix={self.buildEnv.install_dir}"
             if self.buildEnv.configInfo.static:
                 yield "-Dstatic-linkage=true"
