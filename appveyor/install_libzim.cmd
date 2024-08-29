334b1f81cc241d152628d4b08e18e02ddc411b10
Matthieu Gautier <mgautier@kymeria.fr>
2024-08-29 19:14:40 +0200

    Do not build kiwix-desktop using Appveyor CI
    Fix #521, #425

--- appveyor/install_libzim.cmd
+++
@@ -1,9 +0,0 @@
-REM ========================================================
-REM Install libzim
-git clone https://github.com/openzim/libzim.git || exit /b 1
-cd libzim
-meson . build --prefix %EXTRA_DIR% --default-library static --buildtype release || exit /b 1
-cd build
-ninja || exit /b 1
-ninja install || exit /b 1
-cd ..\..
--- appveyor/install_xapian.cmd
+++
@@ -1,9 +0,0 @@
-REM ========================================================
-REM Install xapian
-curl -fsSL -O http://mirror.download.kiwix.org/dev/kiwix-build/xapian-core-1.4.18.zip || exit /b 1
-7z x xapian-core-1.4.18.zip || exit /b 1
-cd xapian-core-1.4.18
-mkdir build
-cd build
-%MINGW64_RUN% "cd /c/Projects/kiwix-build/xapian-core-1.4.18/build && /c/Projects/kiwix-build/appveyor/build_xapian.sh" > build_xapian.log || exit /b 1
-cd ..\..
