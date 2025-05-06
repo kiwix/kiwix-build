d96ee42e0d0a8eaf1e28b2ee5e716b32982a4d75
Veloman Yunkan <veloman.yunkan@gmail.com>
2025-05-06 15:48:53 +0400

    Restore all CI/CD jobs

--- .github/scripts/build_definition.py
+++ .github/scripts/build_definition.py
@@ -17,10 +17,54 @@ import csv, io, re
 BUILD_DEF = """
     | OS_NAME   | COMPILE_CONFIG     | libzim | libkiwix | zim-tools | kiwix-tools | kiwix-desktop | platform_name           | dependency_name        |
     ==============================================================================================================================================
+# manylinux is a special case as we need to compile libzim on old arch for python
+    | manylinux | native_mixed       | BP     |          |           |             |               | linux-x86_64-manylinux  |                        |
+    | manylinux | aarch64_mixed      | BP     |          |           |             |               | linux-aarch64-manylinux |                        |
+    ----------------------------------------------------------------------------------------------------------------------------------------------
+# On Windows, we build only libzim for now. And only native_mixed as xapian doesn't compile as dll
+    | windows   | native_static      | Bd     | d        | BPd       | BPd         |               | win-x86_64              | win-x86_64-static      |
+    | windows   | native_dyn         | Bd     |          |           |             |               | win-x86_64              | win-x86_64-dyn         |
+    | windows   | native_mixed       | BPd    | d        |           |             | BPd           | win-x86_64              | win-x86_64-mixed       |
+    ----------------------------------------------------------------------------------------------------------------------------------------------
 # Osx builds, build binaries on native_dyn and native_static. On anyother things, build only the libraries
-    | macos     | native_dyn         | B      |          |           |             |               |                         | macos-x86_64-dyn       |
-    | macos     | native_static      | B      |          |           |             |               | macos-x86_64            |                        |
-    | macos     | native_mixed       | B      |          |           |             |               | macos-x86_64            |                        |
+    | macos     | native_dyn         | d      | d        | dB        | B           |               |                         | macos-x86_64-dyn       |
+    | macos     | native_static      |        |          | BP        | BP          |               | macos-x86_64            |                        |
+    | macos     | native_mixed       | BP     | BP       |           |             |               | macos-x86_64            |                        |
+    | macos     | iOS_arm64          | dB     | dB       |           |             |               |                         | ios-arm64-dyn          |
+    | macos     | iOSSimulator_x86_64| dB     | dB       |           |             |               |                         | ios-x86_64-dyn         |
+    | macos     | iOSSimulator_arm64 | B      | B        |           |             |               |                         |                        |
+    | macos     | macOS_arm64_static |        |          | BP        | BP          |               | macos-arm64             |                        |
+    | macos     | macOS_arm64_mixed  | dBP    | dBP      | d         |             |               | macos-arm64             | macos-aarch64-dyn      |
+    | macos     | macOS_x86_64       | B      | B        |           |             |               |                         |                        |
+    | macos     | apple_all_static   |        | BP       |           |             |               | xcframework             |                        |
+    ----------------------------------------------------------------------------------------------------------------------------------------------
+    | focal     | flatpak            |        |          |           |             | BP            |                         |                        |
+    | focal     | native_static      | d      | d        | dBPSD     | dBPSD       |               | linux-x86_64            | linux-x86_64-static    |
+    | focal     | native_mixed       | BPS    | BPS      |           |             |               | linux-x86_64            |                        |
+    | focal     | native_dyn         | d      | d        | dB        | dB          |               |                         | linux-x86_64-dyn       |
+    | jammy     | native_dyn         |        |          |           |             | dBPS          |                         | linux-x86_64-dyn       |
+    # libzim CI is building alpine_dyn but not us
+    | focal     | android_arm        | dBP    | dBP      |           |             |               | android-arm             | android-arm            |
+    | focal     | android_arm64      | dBP    | dBP      |           |             |               | android-arm64           | android-arm64          |
+    | focal     | android_x86        | BP     | BP       |           |             |               | android-x86             |                        |
+    | focal     | android_x86_64     | BP     | BP       |           |             |               | android-x86_64          |                        |
+    | focal     | armv6_static       |        |          | BP        | BP          |               | linux-armv6             |                        |
+    | focal     | armv6_mixed        | BP     |          |           |             |               | linux-armv6             |                        |
+    | focal     | armv6_dyn          |        |          | B         | B           |               |                         |                        |
+    | focal     | armv8_static       |        |          | BP        | BP          |               | linux-armv8             |                        |
+    | focal     | armv8_mixed        | BP     |          |           |             |               | linux-armv8             |                        |
+    | focal     | armv8_dyn          |        |          | B         | B           |               |                         |                        |
+    | focal     | aarch64_static     |        |          | BP        | BP          |               | linux-aarch64           |                        |
+    | focal     | aarch64_mixed      | BP     |          |           |             |               | linux-aarch64           |                        |
+    | focal     | aarch64_dyn        | d      |          | B         | B           |               |                         | linux-aarch64-dyn      |
+    | focal     | aarch64_musl_static|        |          | BP        | BP          |               | linux-aarch64-musl      |                        |
+    | focal     | aarch64_musl_mixed | BP     |          |           |             |               | linux-aarch64-musl      |                        |
+    | focal     | aarch64_musl_dyn   | d      |          | B         | B           |               |                         | linux-aarch64-musl-dyn |
+    | focal     | x86-64_musl_static |        |          | BP        | BP          |               | linux-x86_64-musl       |                        |
+    | focal     | x86-64_musl_mixed  | BP     |          |           |             |               | linux-x86_64-musl       |                        |
+    | focal     | i586_static        |        |          | BP        | BP          |               | linux-i586              |                        |
+    | focal     | i586_dyn           |        |          | B         | B           |               |                         |                        |
+    | focal     | wasm               | dBP    |          |           |             |               | wasm-emscripten         | wasm                   |
 """
 
 
