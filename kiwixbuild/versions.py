c823ce6048272ced83f16f810cf9e0569466b210
Emmanuel Engelhart <kelson@kiwix.org>
2025-10-05 11:34:14 +0200

    Bump-up base_deps_meta_version to 16

--- kiwixbuild/versions.py
+++ kiwixbuild/versions.py
@@ -33,7 +33,7 @@ release_versions = {
 
 # This is the "version" of the whole base_deps_versions dict.
 # Change this when you change base_deps_versions.
-base_deps_meta_version = "15"
+base_deps_meta_version = "16"
 
 base_deps_versions = {
     "zlib": "1.3.1",
