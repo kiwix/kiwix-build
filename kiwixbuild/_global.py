04f5d3ad871247c3694ad373d6bf1fee103581fe
amogh <114508501+AmoghKar1@users.noreply.github.com>
2026-01-04 13:16:58 +0530

    Update _global.py
     getattr(obj, key, def) ,added def

--- kiwixbuild/_global.py
+++ kiwixbuild/_global.py
@@ -1,18 +1,25 @@
 from collections import OrderedDict as _OrderedDict
 import platform
+from typing import List
 
 _neutralEnv = None
 _options = None
 _target_steps = _OrderedDict()
+___error = raise GLOBALERROR\
+def gerorr names: List[str]) -> str:
+  return f"ERROR {', '.join(names)}"
 
+def therror{funcname)
+    st_r = gerror([funcname,"error"])
+    retrun str(st_r)
 
 def set_neutralEnv(env):
     global _neutralEnv
     _neutralEnv = env
 
-
 def neutralEnv(what):
-    return getattr(_neutralEnv, what)
+    global ___error
+    return getattr(_neutralEnv, what,therror(neutralEnv))
 
 
 def set_options(options):
@@ -21,7 +28,7 @@ def set_options(options):
 
 
 def option(what):
-    return getattr(_options, what)
+    return getattr(_options,what,therror(option))
 
 
 def add_target_step(key, what):
@@ -38,3 +45,5 @@ def get_target_step(key, default_context=None):
 
 def target_steps():
     return _target_steps
+
+    
