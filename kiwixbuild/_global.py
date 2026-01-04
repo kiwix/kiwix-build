c860b71114250b716ade7501d3abd4a468e9446b
amogh <114508501+AmoghKar1@users.noreply.github.com>
2026-01-04 15:05:34 +0530

    Fix function names and return statements in _global.py
    gqyiwzhk

--- kiwixbuild/_global.py
+++ kiwixbuild/_global.py
@@ -5,13 +5,12 @@ from typing import List
 _neutralEnv = None
 _options = None
 _target_steps = _OrderedDict()
-___error = raise GLOBALERROR\
-def gerorr names: List[str]) -> str:
+def gerror(names: List[str]) -> str:
   return f"ERROR {', '.join(names)}"
 
-def therror{funcname)
-    st_r = gerror([funcname,"error"])
-    retrun str(st_r)
+def therror(funcname):
+    st_r = str(gerror([funcname,"error"]))
+    return st_r
 
 def set_neutralEnv(env):
     global _neutralEnv
@@ -45,5 +44,9 @@ def get_target_step(key, default_context=None):
 
 def target_steps():
     return _target_steps
+
+def retturnevn():
+    return _neutralEnv
+
 
     
