69900ff2550bb242647d0356fdf7d3ced96c2858
Adam Lamar <adamlamar@users.noreply.github.com>
2026-01-26 21:35:32 -0700

    Skip uploads for forks that can't access tmp.kiwix.org

--- .github/scripts/build_projects.py
+++ .github/scripts/build_projects.py
@@ -7,10 +7,13 @@ from common import (
     create_desktop_image,
     fix_macos_rpath,
     upload_archive,
+    should_upload,
     OS_NAME,
     COMPILE_CONFIG,
     DEV_BRANCH,
 )
+
+skip_upload = not should_upload()
 
 for target in select_build_targets(BUILD):
     run_kiwix_build(target, config=COMPILE_CONFIG)
@@ -21,4 +24,4 @@ for target in select_build_targets(BUILD):
             fix_macos_rpath(target)
         archive = make_archive(target, make_release=False)
     if archive and DEV_BRANCH:
-        upload_archive(archive, target, make_release=False, dev_branch=DEV_BRANCH)
+        upload_archive(archive, target, make_release=False, dev_branch=DEV_BRANCH, skip_upload=skip_upload)
--- .github/scripts/upload_failure_logs.py
+++ .github/scripts/upload_failure_logs.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
+import os
 import tarfile
 from pathlib import Path
-from common import upload, OS_NAME, COMPILE_CONFIG, HOME
+from common import upload, should_upload, fork_name, OS_NAME, COMPILE_CONFIG, HOME, print_message
 
 ARCHIVE_NAME = Path(f"fail_log_{OS_NAME}_{COMPILE_CONFIG}.tar.gz")
 
+skip_upload = not should_upload()
+if skip_upload:
+    print_message("Skipping failure log upload for fork: {}", fork_name())
+    exit(0)
 
 files_to_archive = []
 files_to_archive += HOME.glob("BUILD_*")
